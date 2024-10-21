2f792da49386bf14d1c38c6937f62bdcce36f2b6
John <piquagrearj@gmail.com>
2024-10-21 07:32:01 -0400

    Added ground detection by finding all HTML elements tagged with 'ground' before calling the gameLoop in index.js and passing the array of collisionObjects to the Mega Man update() to use whenever moving, jumping, or falling to find any object beneath Mega Man and also within x bounds. When Mega Man gets within 10 (minCollisionDetectionDistance) of the object Y coordinate, adjust the y coordinate and bounds to fit perfectly against the object top.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,13 +4,14 @@ import Time from './utils/time.js';
 import './utils/event-handler.js';
 
 const megaMan = new MegaMan();
+const collisionObjects = [];
 
 function gameLoop() {
     // Update delta time to be used in other classes
     Time.update();
 
-    // Handle all functionality for Mega Man       
-    megaMan.update();
+    // Handle all functionality for Mega Man
+    megaMan.update(collisionObjects);
 
     // Handle all bullet movement
     Bullet.list.forEach(bullet => {
@@ -20,4 +21,11 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
+function findWalkableArea() {
+    var groundTagElements = Array.from(document.getElementsByClassName('ground'));
+
+    groundTagElements.forEach(ground => collisionObjects.push(ground.getBoundingClientRect()))
+}
+
+findWalkableArea();
 gameLoop();
